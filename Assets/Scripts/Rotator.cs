4372cca9668771dab8161ce5ea4150c51385ad1a
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2022-10-05 16:57:37 -0300

    L01 Added

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+
+{
+    public int rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 1 * rotateSpeed * Time.deltaTime); 
+    }
+}
