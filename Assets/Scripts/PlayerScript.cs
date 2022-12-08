b0a48f00c257ea549d99f41f16fbe070f77e4f1d
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2022-12-08 13:32:56 -0400

    Fixed keys not updating in UI

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -17,6 +17,7 @@ public class PlayerScript : MonoBehaviour
     
     void SetKeysText()
     {
+        Debug.Log("This should change the amount of keys.");
         keysAmountText.text = "Keys: " + keys.ToString();
     }
 
@@ -24,10 +25,22 @@ public class PlayerScript : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Pickup"))
         {
+            Debug.Log("The pickup is recognized.");
+            Debug.Log("The pickup will be set inactive.");
+
             other.gameObject.SetActive(false);
-            keys = keys++;
+
+            Debug.Log("The pickup should now be inactive.");
+            Debug.Log("The keys int should now go up by one in the code.");
+
+            keys = keys + 1;
+
+            Debug.Log("The keys int should now be increased by one in the code.");
+            Debug.Log("SetKeysText method will now be called.");
 
             SetKeysText();
+
+            Debug.Log("SetKeysText method has been called and the keys amount shown in the UI should be increased by one.");
         }
     }
 }
